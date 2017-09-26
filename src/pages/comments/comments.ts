3c9466280ded7e9a7b1c281a56263eb54725896e
shadtk <marcosviniciusl.rodrigues@gmail.com>
2017-09-26 20:49:12 -0300

    Created ngfor that generates the comment list, and added it's respective CSS classes

--- src/pages/comments/comments.ts
+++ src/pages/comments/comments.ts
@@ -5,12 +5,26 @@ import { NavController,NavParams} from 'ionic-angular';
   templateUrl: 'comments.html',
 })
 export class Comments {
-
+	private Comments: {date:string, text:string}[] = [];
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+	var d = new Date();
+  	this.Comments.push({date:d.toDateString(), text:"Venham conferir, muito bom!"})
+  	this.Comments.push({date:d.toDateString(), text:"Encontrei um gato bravo em uma caixa, não gostei."})
+  	//console.log(Comments);
+
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad Comments');
   }
 
+	doRefresh(refresher) {
+    console.log('Begin async operation', refresher);
+
+    setTimeout(() => {
+      console.log('Async operation has ended');
+      refresher.complete();
+    }, 2000);
+  }
 }
